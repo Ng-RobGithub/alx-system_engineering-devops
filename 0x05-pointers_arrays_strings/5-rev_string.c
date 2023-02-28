7173a39835e2d3793aa46a17b73a1ea7104afdf6
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-02-27 17:18:33 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: input
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+
+	char rv = s[0];
+	int a = 0;
+	int i;
+
+	while (s[a] != '\0')
+		a++;
+
+	for (i = 0; i < a; i++)
+	{
+		a--;
+		rv = s[i];
+		s[i] = s[a];
+		s[a] = rv;
+	}
+}
