2cf24b2a3ccb626dffadd66be20b9a5e273c807c
Ng-RobGithub <ngrob4real@gmail.com>
2023-10-03 15:20:45 -0700

    a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,4 @@
 #!/usr/bin/env ruby
-# Check if the argument matches the regular expression /School/
-def match_school(argument)
-regex = /School/
-if argument.match?(regex)
-puts "The argument '#{argument}' contains 'School'."
-else
-puts "The argument '#{argument}' does not contain 'School'."
-end
-end
 
-# Check if an argument was provided and call the matching function
-if ARGV.empty?
-puts "Please provide an argument."
-else
-argument = ARGV[0]
-match_school(argument)
-end
+# Check if the argument contains 'School' and print it
+puts ARGV[0].include?('School') ? ARGV[0] : ''
