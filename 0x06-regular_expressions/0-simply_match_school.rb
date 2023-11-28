ef813ab3fdbe270a8dc64b4553d15873cf75f5a6
Ng-RobGithub <ngrob4real@gmail.com>
2023-11-28 10:11:26 -0800

    delete regular expression

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-
-# Check if the argument contains 'School' and print it
-puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-# Check if the argument matches the regular expression pattern
-if ARGV[0] =~ /hb?t?n/
-puts ARGV[0]
-end
