a6c74e9949c2acd4134a0859439d085f3c3ef3b9
Ng-RobGithub <ngrob4real@gmail.com>
2024-02-19 13:53:56 -0800

    using this REST API, for a given employee ID

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,16 +9,20 @@ import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
+
 def main():
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             employee_id = int(sys.argv[1])
-            user_response = requests.get('{}/users/{}'.format(REST_API, employee_id)).json()
+            user_response = requests.get('{}/users/{}'.format(REST_API,
+                                         employee_id)).json()
             tasks_response = requests.get('{}/todos'.format(REST_API)).json()
             employee_name = user_response.get('name')
-            tasks = [task for task in tasks_response if task.get('userId') == employee_id]
+            tasks = [task for task in tasks_response if task.get('userId')
+                     == employee_id]
             completed_tasks = [task for task in tasks if task.get('completed')]
-            print('Employee {} is done with tasks({}/{}):'.format(employee_name, len(completed_tasks), len(tasks)))
+            print('Employee {} is done with tasks({}/{}):'.format(
+                         employee_name, len(completed_tasks), len(tasks)))
             if completed_tasks:
                 for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
