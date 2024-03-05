0df1fdfc924b8b8d1372eb61670ec62651c83b81
Ng-RobGithub <ngrob4real@gmail.com>
2024-03-05 12:07:44 -0800

    queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-'''
+
+"""
     this module contains the function number_of_subscribers
-'''
+"""
 import requests
-from sys import argv
+import sys
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
-        return 0
+    """
+    Returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.ok:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except ValueError:
+            # JSON decoding failed
+            pass
+
+    return 0
 
 
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+# Test the function
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
